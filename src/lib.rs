f77ec3d170ee242d95ddda6b7fc78d346cc5818e
CLOwinnie <winnie@clo3d.com>
2024-07-01 21:14:46 +0900

    web-sys

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,26 @@
+extern crate web_sys;
+use web_sys::console;
 mod utils;
 use std::fmt;
 use wasm_bindgen::prelude::*;
+
+pub struct Timer<'a> {
+    name: &'a str,
+}
+
+impl<'a> Timer<'a> {
+    pub fn new(name: &'a str) -> Timer<'a> {
+        console::time_with_label(name);
+        Timer { name }
+    }
+}
+
+impl<'a> Drop for Timer<'a> {
+    fn drop(&mut self) {
+        console::time_end_with_label(self.name);
+        let _timer = Timer::new("Universe::tick");
+    }
+}
 
 #[wasm_bindgen]
 #[repr(u8)]
